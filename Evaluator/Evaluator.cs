9f33b73d96f8234ab67770de820128c402c563da
iko <trzeja.mikolaj@gmail.com>
2017-11-09 20:39:43 +0100

    poprawne rysowanie regionow, cos nie tak z fukcja zwracajaca hostogramy blokow

--- Evaluator/Evaluator.cs
+++ Evaluator/Evaluator.cs
@@ -23,20 +23,33 @@ namespace Evaluator
         private List<SubRegion> _subRegions;
 
 
+        private IntPtr _ptr; //debug draw
+        private BitmapData _bmpData; //debug draw
+
         public void ProcessImages()
         {
             string path = @"C:\Users\trzej_000\Google Drive\Politechniczne\INZ\lena_gray64.gif";
+
+            //debug
+            //ReadFileWithSaveOption(path);
+            //CreateSubRegions();
+            //MergeSubRegions();
+            //DrawPictureAndSave();
+            //debug
+
 
             ReadFile(path);
             CreateSubRegions();
             var h1 = GetNormalizedHistogramfromFile();
 
-            ReadFile(path);
-            var h2 = GetNormalizedHistogramfromFile();
+            MergeSubRegions();
 
-           // SaveResults(h1);
+            //ReadFile(path);
+            //var h2 = GetNormalizedHistogramfromFile();
 
-            var MSE = CalculateMSE(h1, h2);
+            //SaveResults(h1);
+
+            // var MSE = CalculateMSE(h1, h2);
         }
 
         private void ReadFile(string path)
@@ -48,7 +61,7 @@ namespace Evaluator
                 _bmp.PixelFormat);
 
             IntPtr ptr = bmpData.Scan0;
-                        
+
             _bytes = _bmp.Width * _bmp.Height;            
 
             _greyValues = new byte[_bytes];
@@ -59,6 +72,45 @@ namespace Evaluator
             System.Runtime.InteropServices.Marshal.Copy(ptr, _greyValues, 0, _bytes);
 
             _bmp.UnlockBits(bmpData);
+        }
+
+        private void ReadFileWithSaveOption(string path)
+        {
+            _bmp = new Bitmap(path);
+
+            Rectangle rect = new Rectangle(0, 0, _bmp.Width, _bmp.Height);
+            _bmpData = _bmp.LockBits(rect, ImageLockMode.ReadWrite,
+                _bmp.PixelFormat);
+
+            //IntPtr ptr = bmpData.Scan0;
+            _ptr = _bmpData.Scan0;
+
+            _bytes = _bmp.Width * _bmp.Height;
+
+            _greyValues = new byte[_bytes];
+            _ID = new byte[_bytes];
+
+            _histogram = new double[(Consts.MaxLBP + 1) * Consts.Bins];
+
+            System.Runtime.InteropServices.Marshal.Copy(_ptr, _greyValues, 0, _bytes);
+
+            //_bmp.UnlockBits(bmpData);
+        }
+
+        private void DrawPictureAndSave()
+        {
+            System.Runtime.InteropServices.Marshal.Copy(_greyValues, 0, _ptr, _bytes);
+
+            // Unlock the bits.
+            _bmp.UnlockBits(_bmpData);
+
+            // Draw the modified image.
+            //e.Graphics.DrawImage(bmp, 0, 150);
+            string output = @"C:\Users\trzej_000\Google Drive\Politechniczne\INZ\lena_grayDrawed.gif";
+
+            _bmp.Save(output);
+
+            System.Diagnostics.Process.Start(output);
         }
              
 
@@ -74,13 +126,13 @@ namespace Evaluator
             _blocks = new List<Rectangle>();
             _subRegions = new List<SubRegion>();
 
-            Rectangle mainBlock = new Rectangle(1, 1, _bmp.Width - 2, _bmp.Height - 2);
+            Rectangle mainBlock = new Rectangle(1, 1, _bmp.Width - 1, _bmp.Height - 1);
 
             int id = 0;
 
-            for (int i = mainBlock.Y; i < mainBlock.Height; i += Consts.minimumBlockSize)
+            for (int i = mainBlock.Y; i <= mainBlock.Height; i += Consts.minimumBlockSize)
             {
-                for (int j = mainBlock.X; j < mainBlock.Width; j += Consts.minimumBlockSize)
+                for (int j = mainBlock.X; j <= mainBlock.Width; j += Consts.minimumBlockSize)
                 {
                     int newBlockWidth = Consts.minimumBlockSize;
                     int newBlockHeight = Consts.minimumBlockSize;
@@ -103,30 +155,65 @@ namespace Evaluator
 
                     _subRegions.Add(newSubRegion);
                 }
-            }            
-           
-            //TODO wydzielic jako osobna funkcje (SetSubRegionsNeighbors)
+            }
+
+            SetSubRegionsNeighbors();         
+        }
+
+        private void SetSubRegionsNeighbors()
+        {
             foreach (var region in _subRegions)
             {
                 var block = region.Blocks.FirstOrDefault();
                 //create block enlarged by 1 in each direction
                 var enlargedBlock = new Rectangle(block.X - 1, block.Y - 1, Consts.minimumBlockSize + 2, Consts.minimumBlockSize + 2);
-                
+
                 var regionNeighbors = _subRegions
                     .Where(s => s.Blocks.FirstOrDefault().IntersectsWith(enlargedBlock) && !s.Equals(region))
                     .ToList();
 
                 region.Neighbors = regionNeighbors;
-
-            }            
+            }
         }
 
-        private double[] GetNormalizedHistogramFrom(Rectangle block)
+        private void MergeSubRegions()
         {
-            for (int i = block.Y; i < block.Height; i++)
+            //var histograms = new List<double[]>();
+            double [][] histograms  = new double[16][];
+            int i = 0;
+
+            //byte color = 255;
+            //na razie policz histogrmay dla wszystkich bokow <-dzialalo
+            foreach (var region in _subRegions)
             {
-                for (int j = block.X; j < block.Width; j++)
+                var block = region.Blocks.FirstOrDefault();
+
+                var h = GetNormalizedHistogramFrom(block/*, color*/);
+                //histograms.Add(h);
+                histograms[i++] = h;
+                //color -= 10;
+            }
+
+            double MSE = 0;
+
+            
+            for (int k = 0; k < 14; k++)
+            {
+                MSE = CalculateMSE(histograms[k], histograms[k+1]);
+            }
+            //TODO bloki sa tworzone w dobrych lokalizacjach (draw)
+            //jednak MSE miedzy wzystkimi = 0, czemu? czyzby za kazdym razem dostawal ten sam histogram? cos nie tak z 
+            //funkcja GetNormalizedHistogram pewno
+
+        }
+
+        private double[] GetNormalizedHistogramFrom(Rectangle block, byte color=0)
+        {
+            for (int i = block.Y; i < block.Y + block.Height; i++)
+            {
+                for (int j = block.X; j < block.X + block.Width; j++)
                 {
+                    //_greyValues[_bmp.Width * i + j] = color;//debug
                     var LBPC = HelperMethods.CountLBPC(_greyValues, _bmp.Width, _bmp.Width * i + j);                    
                     int b = HelperMethods.GetBinFor(LBPC.C);
 
@@ -134,7 +221,7 @@ namespace Evaluator
                 }
             }
 
-            NormalizeHistogram(_histogram, _bmp.Width * _bmp.Height);
+            NormalizeHistogram(_histogram, block.Width * block.Height);
 
             return _histogram;
         }
