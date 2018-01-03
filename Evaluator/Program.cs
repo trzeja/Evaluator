57393ef49001e154bdb24f8d3a05bf7f43a5ff38
iko <trzeja.mikolaj@gmail.com>
2018-01-03 21:02:12 +0100

    Drobna walidacja liczby parametrow

--- Evaluator/Program.cs
+++ Evaluator/Program.cs
@@ -12,22 +12,29 @@ namespace Evaluator
     {
         static void Main(string[] args)
         {
-            string resultsFilePath = args[0];
-            string image1FilePath = args[1];
-            string image2FilePath = args[2];
-            
-            if (File.Exists(image1FilePath) && File.Exists(image2FilePath))
+            if (args.Count() != 3)
             {
-                var evaluator = new Evaluator();
-                var PSNR = evaluator.CalculatePSNR(image1FilePath, image2FilePath);
-                var similarity = evaluator.CalculateSimilarityBySegmentation(image1FilePath, image2FilePath);
-  
-                SaveResults(PSNR, similarity, resultsFilePath);
+                Console.WriteLine("Incorrect number of parameters (should be 3)");
             }
             else
             {
-                Console.WriteLine("Incorrect path - image file not found");
-            }            
+                string resultsFilePath = args[0];
+                string image1FilePath = args[1];
+                string image2FilePath = args[2];
+
+                if (File.Exists(image1FilePath) && File.Exists(image2FilePath))
+                {
+                    var evaluator = new Evaluator();
+                    var PSNR = evaluator.CalculatePSNR(image1FilePath, image2FilePath);
+                    var similarity = evaluator.CalculateSimilarityBySegmentation(image1FilePath, image2FilePath);
+
+                    SaveResults(PSNR, similarity, resultsFilePath);
+                }
+                else
+                {
+                    Console.WriteLine("Incorrect path - image file not found");
+                }
+            }                  
         }
 
         static void SaveResults(double PSNR, double similarity, string path)
